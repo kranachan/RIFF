f9df0c85ae87a5ec43c53b7fceaf385540741062
Ayane <necromancer@minakami.land>
2023-02-05 12:52:43 +0800

    feat: configure theme for tailwind

---
+++ src/theme/index.ts
@@ -0,0 +1,39 @@
+import { defaultTheme } from './default'
+import { ColorScheme, Theme } from '@/constants/theme'
+
+export const hex2rgb = (hexColor: string) => {
+  if (!hexColor.startsWith('#')) {
+    return '255 255 255'
+  }
+
+  const arrayedHexColor = hexColor.split('')
+  arrayedHexColor.shift()
+  const pureHex = arrayedHexColor.join('')
+  const bigint = parseInt(pureHex, 16)
+  const r = (bigint >> 16) & 255
+  const g = (bigint >> 8) & 255
+  const b = bigint & 255
+
+  return [r, g, b].join(' ')
+}
+
+export const setColor2PropertyByHex = (
+  name: string,
+  hexColor: string,
+): void => {
+  document.documentElement.style.setProperty(
+    `--colors-${name}`,
+    hex2rgb(hexColor),
+  )
+}
+
+export const applyTheme = (
+  theme: Theme,
+  scheme: ColorScheme.Light | ColorScheme.Dark,
+) => {
+  Object.entries(theme[scheme]).forEach(([propertyName, hexColor]) => {
+    setColor2PropertyByHex(propertyName, hexColor)
+  })
+}
+
+export { defaultTheme }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/*.{vue,js,ts}', './src/**/*.{vue,js,ts}'],
+  darkMode: 'jit',
   theme: {
-    extend: {},
+    colors: {
+      primary: 'rgb(var(--colors-primary))',
+      foreground: 'rgb(var(--colors-foreground))',
+      background: 'rgb(var(--colors-background))',
+      surface: 'rgb(var(--colors-surface))',
+      placeholder: 'rgb(var(--colors-placeholder))',
+      hint: 'rgb(var(--colors-hint))'
+    },
   },
   plugins: [],
 }
