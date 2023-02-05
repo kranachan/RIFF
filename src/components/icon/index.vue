d726eee6084eaa511af7ba5da03c38d77a0754f1
Charles <446450060@qq.com>
2023-02-05 17:34:58 +0800

    feat: add `Icon` component

---
+++ src/components/icon/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed, withDefaults } from 'vue'
+import * as icons from 'lucide-vue-next'
+
+const props = withDefaults(
+  defineProps<{
+    name: string
+    size?: number
+    color?: string
+    strokeWidth?: number
+    defaultClass?: string
+  }>(),
+  {
+    size: 24,
+    color: 'currentColor',
+    strokeWidth: 2,
+    defaultClass: 'lucide-icon',
+  },
+)
+
+const icon = computed(
+  () => (icons as { [key: string]: icons.Icon })[props.name],
+)
+</script>
+
+<template>
+  <component
+    :is="icon"
+    :size="size"
+    :color="color"
+    :stroke-width="strokeWidth"
+    :default-class="defaultClass"
+  />
+</template>
