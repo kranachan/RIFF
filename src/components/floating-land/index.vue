b18a5c050fa78f5cd170ca618852365bffe835d3
Ayane <necromancer@minakami.land>
2023-02-05 15:02:09 +0800

    feat: add `FloatingLand` component

---
+++ src/components/floating-land/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { gsap } from 'gsap'
+
+const floatingAnim = gsap.timeline()
+
+onMounted(() => {
+  floatingAnim
+    .fromTo('.root', { bottom: -200 }, { bottom: 5, duration: 0.8 })
+    .to('.root', { bottom: 0, ease: 'elastic' })
+})
+</script>
+
+<template>
+  <div class="root">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
