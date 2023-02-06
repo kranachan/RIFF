d4acc5977c9ca980771e237d97cba336edb8dce5
Charles <446450060@qq.com>
2023-02-06 14:42:48 +0800

    feat: use router for `BottomNavigation`

---
+++ src/pages/main/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import BottomNavigation from '@/components/bottom-navigation/index.vue'
+</script>
+
+<template>
+  <RouterView />
+  <BottomNavigation />
+</template>
