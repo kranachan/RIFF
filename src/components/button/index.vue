565c5b268ba08648460220226b0d91de1c73f235
Charles <446450060@qq.com>
2023-02-09 23:06:18 +0800

    feat: add `Button` component

---
+++ src/components/button/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button">
+    <span class="button-text"><slot /></span>
+  </button>
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
