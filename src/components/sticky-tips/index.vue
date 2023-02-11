2944f8398509f43587cea27b2a4d18b7b4998ab5
Charles <446450060@qq.com>
2023-02-11 22:09:13 +0800

    feat: add `StickyTips` component

---
+++ src/components/sticky-tips/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Icon from '@/components/icon/index.vue'
+import Polygon from '@/assets/icons/polygon.svg?component'
+</script>
+
+<template>
+  <div class="sticky-wrapper">
+    <slot />
+    <div class="sticky-tips">
+      <div class="icon"><Icon name="PlusSquare" /></div>
+      <div class="text">add-photo</div>
+      <Polygon class="arrow" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
