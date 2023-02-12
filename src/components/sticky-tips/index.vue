867e2a596f678487529daaad7b27249cd52f547d
Charles <446450060@qq.com>
2023-02-12 15:03:57 +0800

    feat: add `StickyTips` component

---
+++ src/components/sticky-tips/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { withDefaults } from 'vue'
+import Icon from '@/components/icon/index.vue'
+import Polygon from '@/assets/icons/polygon.svg?component'
+
+const props = withDefaults(
+  defineProps<{
+    title: string
+    icon?: string
+  }>(),
+  {
+    icon: '',
+  },
+)
+</script>
+
+<template>
+  <div class="sticky-wrapper">
+    <slot />
+    <div class="sticky-tips">
+      <div class="icon" v-if="icon"><Icon :name="props.icon" /></div>
+      <div class="text">{{ props.title }}</div>
+      <Polygon class="arrow" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
