1cc8f1e3d085059aabf63bc62961532ad6a1055e
Ayane <necromancer@minakami.land>
2023-02-14 06:31:38 +0800

    feat: add `avatar` component

---
+++ src/components/avatar/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { source, size } = withDefaults(
+  defineProps<{
+    source: string | null
+    size?: number
+  }>(),
+  {
+    size: 40,
+  },
+)
+
+const sizeWithUnit = `${size}px`
+const style = { width: sizeWithUnit, height: sizeWithUnit }
+</script>
+
+<template>
+  <img class="avatar" v-lazy="source" :style="style" />
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
