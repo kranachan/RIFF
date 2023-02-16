c68e663c883970d6b2e00e6ceaceddec6623999f
Charles <446450060@qq.com>
2023-02-16 23:27:43 +0800

    feat: add `PhotoCard` component

---
+++ src/components/photo-card/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Avatar from '@/components/avatar/index.vue'
+</script>
+
+<template>
+  <div class="photo-card">
+    <img class="cover" />
+    <div class="info">
+      <Avatar source="" :size="24" />
+      <div class="nickname">[name]</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
