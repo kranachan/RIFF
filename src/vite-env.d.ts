cf741fd53204cc3d08897818f0331beb2482412d
Ayane <me@ayane.io>
2023-03-30 22:29:43 +0800

    chore: configure env and dev server proxy

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_API_URL: string
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
+const DEFAULT_API_URL = 'https://api.riff.rocks/v1'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), svgLoader()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()))
+
+  return {
+    plugins: [vue(), svgLoader()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL ?? DEFAULT_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
