114d913b950f9397d4bc75094c18830c67fb786c
Ayane <me@ayane.io>
2023-03-31 23:06:29 +0800

    feat: add `TextField` component

---
+++ src/components/text-field/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  },
+})
+</script>
+
+<template>
+  <input class="text-field" type="text" v-model="value" />
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
