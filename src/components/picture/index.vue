008eaace159dd43c72840e26dd459f10adb77f85
Ayane <necromancer@minakami.land>
2023-03-31 23:06:06 +0800

    feat: add `Picture` component

---
+++ src/components/picture/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  pngSource?: string
+  jpgSource?: string
+  webpSource?: string
+}>()
+</script>
+
+<template>
+  <picture>
+    <source v-if="webpSource" :srcset="webpSource" />
+    <source v-if="pngSource" :srcset="pngSource" />
+    <source v-if="jpgSource" :srcset="jpgSource" />
+    <img :src="pngSource ?? jpgSource" />
+  </picture>
+</template>
+
+<style scoped>
+@import url('./index.css');
+</style>
