920843b2562c0147e16fc0978ff0607639c1f725
Ayane <necromancer@minakami.land>
2023-04-02 07:43:29 +0800

    fix(workflow): hash mode env variable

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router'
 import { routes } from './routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: import.meta.env.VITE_ENABLE_HASH_MODE
+    ? createWebHashHistory(import.meta.env.BASE_URL)
+    : createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 })
