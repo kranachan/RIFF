c47fcef1302202bdde9adeb8c49b579152334023
Ayane <necromancer@minakami.land>
2023-04-02 08:08:37 +0800

    fix(store): self type

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,9 +1,9 @@
-import { User } from '@/models/user'
+import { Self } from '@/models/user'
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
 const useAppStore = defineStore('app', () => {
-  const self = ref<User>()
+  const self = ref<Self>()
   const isLoggedIn = computed(() => !!self.value)
   return { self, isLoggedIn }
 })
