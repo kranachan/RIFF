c47440826b949740f12091cdda4c63fa09b8fba3
Ayane <me@ayane.io>
2023-04-15 16:41:16 +0800

    refactor: default request with token

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
 import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import { ColorScheme } from '@/constants/theme'
+import { validateToken } from '@/utils'
 import { applyTheme, defaultTheme } from '@/theme/index'
 import { AuthApi } from './api/auth'
 import { useStore } from './store'
-import { getLocalStorage } from './utils'
 import { Toaster } from 'vue-sonner'
 
 const { app } = useStore()
 
 onMounted(async () => {
   applyTheme(defaultTheme, ColorScheme.Light)
-  const token = await getLocalStorage('token')
-  if (!token) {
+  const isValidToken = await validateToken()
+  if (!isValidToken) {
     return
   }
-  const self = await AuthApi.loginByToken(token)
+  const self = await AuthApi.loginByToken()
   app.self = self
 })
 </script>
