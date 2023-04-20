a2ce6ad4c8c824901ef07afe6712e9b737f7242c
Ayane <necromancer@minakami.land>
2023-04-20 09:24:38 +0800

    fix(floating-land): using `@vueuse/motion` as a replacement for gsap

--- src/components/floating-land/FloatingLand.vue
+++ src/components/floating-land/FloatingLand.vue
@@ -1,18 +1,12 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { gsap } from 'gsap'
-
-const floatingAnim = gsap.timeline()
-
-onMounted(() => {
-  floatingAnim
-    .fromTo('.root', { bottom: -200 }, { bottom: 5, duration: 0.8 })
-    .to('.root', { bottom: 0, ease: 'elastic' })
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="root">
+  <div
+    class="root"
+    v-motion
+    :initial="{ bottom: -200 }"
+    :enter="{ bottom: 5, transition: { duration: 500 } }"
+  >
     <slot />
   </div>
 </template>
