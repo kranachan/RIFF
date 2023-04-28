9a2c7e1434725793581c6f237419abd021f52e8c
Ayane <me@ayane.io>
2023-04-28 21:00:13 +0800

    refactor: revamp user page

--- src/pages/mobile/user/User.vue
+++ src/pages/mobile/user/User.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useElementVisibility } from '@vueuse/core'
 import Tabbar from '@/components/tabbar/Tabbar.vue'
 import Button from '@/components/button/Button.vue'
 import Icon from '@/components/icon/Icon.vue'
 import PageLayout from '@/components/page-layout/PageLayout.vue'
+import { useGoBack } from '@/hooks'
+import { useRoute } from 'vue-router'
+import { useStore } from '@/store'
+import Avatar from '@/components/avatar/Avatar.vue'
 
-const anchorRef = ref<HTMLDivElement>()
+const infoAnchorRef = ref<HTMLDivElement>()
+const isInfoAnchorVisible = useElementVisibility(infoAnchorRef)
 
-const anchorVisible = useElementVisibility(anchorRef)
+const route = useRoute()
+const { userStore } = useStore()
+const userId = route.params.userId as string
+const user = computed(() => userStore.getUserById(userId))
+
+onMounted(async () => {
+  if (!user.value) {
+    await userStore.fetchUserById(userId)
+  }
+})
+
+const onBackClicked = useGoBack()
 
 const list = [
   {
@@ -36,50 +52,49 @@ const list = [
 
 <template>
   <PageLayout class="user">
-    <div class="header" :class="anchorVisible && 'transparent'">
-      <button class="button"><Icon name="ChevronLeft" /></button>
-      <div class="info">
-        <div class="info-wrapper">
-          <div class="info-avatar"></div>
-          <div class="info-right">
-            <div class="info-nickname">[name]</div>
-            <div class="info-datetime">[information]</div>
-          </div>
-        </div>
+    <div class="header" :class="isInfoAnchorVisible && 'is-info-in-view'">
+      <button class="header-action" @click="onBackClicked">
+        <Icon name="ChevronLeft" />
+      </button>
+      <div class="header-info">
+        <Avatar :size="32" />
+        <section class="header-info-profile">
+          <div class="header-info-name">{{ user?.name }}</div>
+          <div class="header-info-bio">{{ user?.bio }}</div>
+        </section>
       </div>
-      <button class="button"><Icon name="MoreVertical" /></button>
+      <button class="header-action ml-auto">
+        <Icon name="MoreVertical" />
+      </button>
     </div>
 
     <div class="user-cover">
-      <img alt="banner" />
+      <img v-if="user?.banner" :src="user.banner" alt="banner" />
     </div>
 
     <div class="user-profile">
       <div class="head">
-        <div class="avatar"></div>
+        <Avatar :source="user?.avatar" :size="64" class="avatar" />
         <div class="stats">
           <div class="stat">
             <span class="stat-label">Following</span>
-            <span class="stat-count">122</span>
+            <span class="stat-count">{{ user?.followingCount ?? 0 }}</span>
           </div>
           <div class="stat">
             <span class="stat-label">Follower</span>
-            <span class="stat-count">3.4k</span>
+            <span class="stat-count">{{ user?.followerCount ?? 0 }}</span>
           </div>
         </div>
         <Button>Follow</Button>
       </div>
-      <div class="nickname">[name]</div>
-      <div class="sign">[information]</div>
+      <div ref="infoAnchorRef" />
+      <div class="nickname">{{ user?.name }}</div>
+      <div class="bio" v-if="user?.bio">{{ user.bio }}</div>
     </div>
-
-    <div class="tabbar-area">
-      <div class="tarbar" :class="!anchorVisible && 'is-fixed'">
-        <Tabbar :list="list" :active-key="1" />
-      </div>
-      <div class="anchor" ref="anchorRef" />
+    <div class="tabbar-area" :class="!isInfoAnchorVisible && 'is-sticky'">
+      <Tabbar :list="list" :active-key="1" />
     </div>
-    <div class="list-area"></div>
+    <div class="timeline-area"></div>
   </PageLayout>
 </template>
 
